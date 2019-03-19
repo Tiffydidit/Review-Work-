175844fbb7572a47acc2c73f679b5d2e5a8726b8
Tiffydidit <tiffydidit@gmail.com>
2019-03-19 17:07:41 -0400

    This is  the average homework

--- w1d1.js
+++ w1d1.js
@@ -1,29 +1,29 @@
-// let a = 9;
-// let b = 11;
-// let z = a + b;
-// console.log(z);
+let a = 9;
+let b = 11;
+let z = a + b;
+console.log(z);
 
-// let one = 1; 
-// let two = 12; 
-// let three = 18;
-// let four = 4; 
-// let five = 9; 
-// let six = 6; 
-// let seven = 19;
-// let eight  = 9; 
-// let nine = 6; 
-// let ten = 30;
+let one = 1; 
+let two = 12; 
+let three = 18;
+let four = 4; 
+let five = 9; 
+let six = 6; 
+let seven = 19;
+let eight  = 9; 
+let nine = 6; 
+let ten = 30;
 
-// let Avg = (one + two + three + four + five)/5;
+let Avg = (one + two + three + four + five)/5;
 
-// let avg = (six + seven + eight + nine + ten)/5;
+let avg = (six + seven + eight + nine + ten)/5;
 
-// console.log(Avg);
-// console.log(avg);
+console.log(Avg);
+console.log(avg);
 
-// How would we find the average of 5 of ten numbers without declaring ten variables? 
+How would we find the average of 5 of ten numbers without declaring ten variables? 
 
-//USE A LOOP!!!
+USE A LOOP!!!
 
 let Array = [1, 12, 18, 4, 9, 6, 19, 9, 6, 30]; //Put the values in an array.
 
@@ -40,7 +40,7 @@ let average = sum / 5;
 console.log("average: ")
 console.log(average);
 
-
+//AVERAGE HOMEWORK:
 
 let numbers = [4, 10, 22, -30, 55]; 
 
@@ -50,8 +50,20 @@ for(let i = 0; i < 5; i++){
 	add += numbers[i];
 }
 
-let 
+let divide = add / 5;
 
-console.log()
+console.log(divide);
+
+let equal = 0;
 
 let integers = [68, 7, -22, 9, 100];
+
+for (let i = 0; i < integers.length; i++){
+	equal += integers[i];
+}
+
+let Average = equal / 5;
+
+console.log(Average);
+
+
