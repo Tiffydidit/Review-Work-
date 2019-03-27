bb89bc70b4cca8c66488609091e6f307cb17dcb7
Tiffydidit <tiffydidit@gmail.com>
2019-03-27 11:44:26 -0400

    Eloquent JS Exercises

--- eloquentJs.js
+++ eloquentJs.js
@@ -134,6 +134,13 @@ let min = (a,b) => Math.min(a,b);// ES6 function value that uses Math.min functi
 
 console.log(min(0, 10));
 
+/*Write a function countBs that takes a string as its only argument and returns 
+a number that indicates how many uppercase “B” characters there are in the string.
+
+Next, write a function called countChar that behaves like countBs, 
+except it takes a second argument that indicates the character that is to be counted 
+(rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.*/
+
 
 function countBs(string){
 	let i = 0;
