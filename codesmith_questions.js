d17ce663d2a5a5c05adf67189b4d2a874acb275b
Tiffydidit <tiffydidit@gmail.com>
2019-03-28 18:30:05 -0400

    Update

--- codesmith_questions.js
+++ codesmith_questions.js
@@ -26,3 +26,27 @@ for(i in sumMe){
     total += sumMe[i];
   }
 }
+
+//They used an object and the two  .toLowercase(), before I saw there solution I thought to use an  array and nested loops. 
+//Is this doable? 
+
+function disemvowel(string) {
+  // your code here...
+  var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  var result = " ";
+  var i = 0
+  while(i < string.length){
+    for(let j = 0; j < vowels.length; j++){
+      if(string[i] != vowels[j]){
+        result += string[i]
+      }
+    }
+     i++
+  }
+}
+
+//Comment in the code below to test your function:
+
+console.log(disemvowel('CodeSmith')); // => 'CdSmth'
+ console.log(disemvowel('BANANA')); // => 'BNN'
+console.log(disemvowel('hello world')); // => 'hll wrld'
