4e6855aaf4cfbc0348f099d3388b4060052c188b
Tiffydidit <tiffydidit@gmail.com>
2019-03-28 18:34:59 -0400

    Codesmith Challenges that drew questions

--- codesmith_questions.js
+++ codesmith_questions.js
@@ -36,12 +36,13 @@ function disemvowel(string) {
   var result = " ";
   var i = 0
   while(i < string.length){//while i is less than the length of the string. 
-    for(let j = 0; j < vowels.length; j++){
-      if(string[i] != vowels[j]){
-        result += string[i]
+    for(let j = 0; j < vowels.length; j++){//I want to drop into this for loop and iterate through every index of vowels using j
+      if(string[i] !== vowels[j]){//comparing the letter in string to each letter in vowels
+        result += string[i]//if the letter being compared from string is not deeply equal to a letter from vowel, 
+       					   //add it to result and break the loop. 
       }
     }
-     i++
+     i++//increment i to compare the next letter in the string. 
   }
 }
 
