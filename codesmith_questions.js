55a52c97fd7e07db16a2bdbacff20729500ad677
Tiffydidit <tiffydidit@gmail.com>
2019-03-28 17:04:05 -0400

    Codesmith Challenges that drew Questions.

--- codesmith_questions.js
+++ codesmith_questions.js
@@ -1,3 +1,15 @@
+const possibleIterable = [4, 3, 9, 6, 23];
+const divByThree = {};
+// ADD CODE HERE
+
+for(let i = 0; i < possibleIterable.length; i++){
+  	if(possibleIterable[i] % 3 === 0){
+       divByThree[possibleIterable[i]] = i;//why doesn't dot notation work here?
+       }
+}
+console.log(divByThree)
+
+
 const sumMe = {
   hello: 'there',
   you: 8,
