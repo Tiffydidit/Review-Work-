77e89505764f29299e8cbbf706fb883dbbeb7994
Tiffydidit <tiffydidit@gmail.com>
2019-03-29 14:52:54 -0400

    Register

--- js/main.js
+++ js/main.js
@@ -6,67 +6,76 @@ var bills = ["images/hundred.jpg", "images/fifty.png", "images/twenty.jpg",
 
 var img = document.createElement("IMG");
 
-function myMoneyBack(n, price = 4){
-	let n = Number(document.getElementById("n").value);
+function myMoneyBack(paid, price = 4){
+	let num = Number(document.getElementById("n").value);
 	let myChange = 0.00;
-	myChange = n - 4;
+	myChange = num - 4;
 	let i = 0;
 	
 	while (myChange >= 100){
 		myChange -= 100;
 		img.setAttribute("src", bills[i]);
 	}
-	i++;
+	
 
 	while (myChange >= 50){ 
+		i = 1;
 		myChange -= 50; 
 		img.setAttribute("src", (bills[i]));
 	}  
-	i++;
+	
 
 	while (myChange >= 20){
+		i = 2;
 		myChange -= 20;
 		img.setAttribute("src", (bills[i]));
 	}
-	i++;
+	
 
 	while (myChange >= 10){
+		i = 3;
 		myChange -= 10;
 		img.setAttribute("src", (bills[i]));
 	} 
-	i++;
+	
 
 	while (myChange >= 5){
+		i = 4;
 		myChange -= 5;
 		img.setAttribute("src", (bills[i]));
 	} 
-	i++;
+	
 
 	while (myChange >= 1){
+		i = 5;
 		myChange -= 1;
 		img.setAttribute("src", (bills[i]));
 	}
-	i++; 
+	
 
 	while (myChange >= .25){
+		i = 6;
 		myChange -= .25;
 		img.setAttribute("src", (bills[i]));
 	} 
-	i++;
+	
 
 	while (myChange >= .10){
+		i = 7;
 		myChange -= .10;
 		img.setAttribute("src", (bills[i]));
 	} 
-	i++;
+	
 
 	while (myChange >= .05){
+		i = 8;
 		myChange -= .05;
 		img.setAttribute("src", (bills[i]));
 	} 
-	i++;
+	
 
 	while (myChange >= .01){
+		i = 9;
 		myChange -= .01;
 		img.setAttribute("src", (bills[i]));
 	}
