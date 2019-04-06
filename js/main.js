368a6a80745fc77d26206aad1276dedf0be91ee8
Tiffydidit <tiffydidit@gmail.com>
2019-04-06 13:26:38 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -28,66 +28,66 @@ function myMoneyBack( price = 4){
 	}
 	
 
-	// while (myChange >= 50){ 
-	// 	i = 1;
-	// 	myChange -= 50; 
-	// 	img.setAttribute("src", (bills[i]));
-	// }  
+	while (myChange >= 50){ 
+		i = 1;
+		myChange -= 50; 
+		img.setAttribute("src", (bills[i]));
+	}  
 	
 
-	// while (myChange >= 20){
-	// 	i = 2;
-	// 	myChange -= 20;
-	// 	img.setAttribute("src", (bills[i]));
-	// }
+	while (myChange >= 20){
+		i = 2;
+		myChange -= 20;
+		img.setAttribute("src", (bills[i]));
+	}
 	
 
-	// while (myChange >= 10){
-	// 	i = 3;
-	// 	myChange -= 10;
-	// 	img.setAttribute("src", (bills[i]));
-	// } 
+	while (myChange >= 10){
+		i = 3;
+		myChange -= 10;
+		img.setAttribute("src", (bills[i]));
+	} 
 	
 
-	// while (myChange >= 5){
-	// 	i = 4;
-	// 	myChange -= 5;
-	// 	img.setAttribute("src", (bills[i]));
-	// } 
+	while (myChange >= 5){
+		i = 4;
+		myChange -= 5;
+		img.setAttribute("src", (bills[i]));
+	} 
 	
 
-	// while (myChange >= 1){
-	// 	i = 5;
-	// 	myChange -= 1;
-	// 	img.setAttribute("src", (bills[i]));
-	// }
+	while (myChange >= 1){
+		i = 5;
+		myChange -= 1;
+		img.setAttribute("src", (bills[i]));
+	}
 	
 
-	// while (myChange >= .25){
-	// 	i = 6;
-	// 	myChange -= .25;
-	// 	img.setAttribute("src", (bills[i]));
-	// } 
+	while (myChange >= .25){
+		i = 6;
+		myChange -= .25;
+		img.setAttribute("src", (bills[i]));
+	} 
 	
 
-	// while (myChange >= .10){
-	// 	i = 7;
-	// 	myChange -= .10;
-	// 	img.setAttribute("src", (bills[i]));
-	// } 
+	while (myChange >= .10){
+		i = 7;
+		myChange -= .10;
+		img.setAttribute("src", (bills[i]));
+	} 
 	
 
-	// while (myChange >= .05){
-	// 	i = 8;
-	// 	myChange -= .05;
-	// 	img.setAttribute("src", (bills[i]));
-	// } 
+	while (myChange >= .05){
+		i = 8;
+		myChange -= .05;
+		img.setAttribute("src", (bills[i]));
+	} 
 	
 
-	// while (myChange >= .01){
-	// 	i = 9;
-	// 	myChange -= .01;
-	// 	img.setAttribute("src", (bills[i]));
-	// }
+	while (myChange >= .01){
+		i = 9;
+		myChange -= .01;
+		img.setAttribute("src", (bills[i]));
+	}
 	
 }
