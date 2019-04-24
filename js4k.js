ad4183dc24c981f126632b6186d732ce8ab2fae6
Tiffydidit <tiffydidit@gmail.com>
2019-04-24 01:25:57 -0400

    Array Methods

--- js4k.js
+++ js4k.js
@@ -4,14 +4,17 @@ let arr = ["Tiffany", "Joyel", "Warren", "Soyinka", "Ubu", "Roi",];
 let Arr = ["Tiff", "Joy", "War", "inka", "U", "R",];
 let aRR = ["fany", "el", "ren", "Soy", "bu", "oi",];
 
+//concatenate arrays
 let nu = arr.concat(Arr, aRR);
 console.log(nu);
+
+//find the index of an item in an array
 console.log(arr.indexOf("Tiffany"));
 
 
-
-
-
+//turn array into a string. 
+nu = arr.join(" ");
+console.log(nu);
 
 
 
