a674c17f0320ca49d2a9216ecb59cb3382d98432
Tiffydidit <tiffydidit@gmail.com>
2019-04-24 01:21:49 -0400

    Array Methods

---
+++ js4k.js
@@ -0,0 +1,21 @@
+//Array Work/Methods
+
+let arr = ["Tiffany", "Joyel", "Warren", "Soyinka", "Ubu", "Roi",];
+let Arr = ["Tiff", "Joy", "War", "inka", "U", "R",];
+let aRR = ["fany", "el", "ren", "Soy", "bu", "oi",];
+
+let nu = arr.concat(Arr, aRR);
+console.log(nu);
+
+
+
+
+
+
+
+
+
+
+
+
+
