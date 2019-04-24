71f29bd9a94f784acca5b765d600ed4f7b9c9f1d
Tiffydidit <tiffydidit@gmail.com>
2019-04-24 01:38:57 -0400

    Built-in Methods

--- js4k.js
+++ js4k.js
@@ -19,8 +19,16 @@ let nu = arr.concat(Arr, aRR);
 let randoNdx = Math.floor(Math.random() * arr.length);
 
 console.log(arr[randoNdx]);
-
-
-
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
+console.log(arr[randoNdx]);
 
 
